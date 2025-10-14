ac3717c53fd3dd07125b45dae05f7c270d05d1bf
Mariia Pidlisna <mariia.pidlisna@gmail.com>
2025-10-14 09:09:36 +0300

    first push

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const heroes = [
+  {
+    name: "АСТРАХАНЦЕВ ОЛЕКСАНДР",
+    img: "./pages/astrakhantsev-oleksandr/IMG_9682.webp",
+    link: "./pages/astrakhantsev-oleksandr/astrakhantsev-oleksandr.html",
+    desc: `
+      МАЙСТЕР КІОКУШИНКАЙ КАРАТЕ <br>
+      НАРОДИВСЯ 1969 РОКУ В М. БЕРДИЧІВ
+    `,
+  },
+  {
+    name: "КУЛЬКОВ СЕРГІЙ",
+    img: "./pages/kulkov-sergiy/kulkov-sergiy.webp",
+    link: "./pages/kulkov-sergiy/kulkov-sergiy.html",
+    desc: `
+      СПОРТСМЕН КІОКУШИНКАЙ КАРАТЕ ТА СПЕЦПРИЗНАЧИНЕЦЬ <br>
+      НАРОДИВСЯ 1979 РОКУ.`,
+  },
+   {
+    name: "КУЛЬКОВ СЕРГІЙ",
+    img: "./pages/kulkov-sergiy/kulkov-sergiy.webp",
+    link: "./pages/kulkov-sergiy/kulkov-sergiy.html",
+    desc: `
+      СПОРТСМЕН КІОКУШИНКАЙ КАРАТЕ ТА СПЕЦПРИЗНАЧИНЕЦЬ <br>
+      НАРОДИВСЯ 1979 РОКУ.`,
+  },
+];
+
+
+const gallery = document.querySelector(".gallery-ul");
+
+heroes.forEach((hero) => {
+  const li = document.createElement("li");
+  li.classList.add("gallery-list");
+
+  li.innerHTML = `
+    <a class="gallery-list-url" href="${hero.link}">
+      <img src="${hero.img}" alt="${hero.name}">
+      <h2 class="gallery-title-img">${hero.name}</h2>
+      <p>${hero.desc}</p>
+    </a>
+  `;
+
+  gallery.appendChild(li);
+});
