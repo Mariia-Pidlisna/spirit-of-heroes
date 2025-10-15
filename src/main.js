2e6c8ed246aeb7990762283b64435f575dc2932a
Mariia Pidlisna <mariia.pidlisna@gmail.com>
2025-10-15 09:31:50 +0300

    add pages

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
 import IMG_9682 from '/pages/astrakhantsev-oleksandr/IMG_9682.webp';
-import astrakhantsevOleksandr from '/pages/astrakhantsev-oleksandr/astrakhantsev-oleksandr.html';
-import kulkovSergiyImg from '/pages/kulkov-sergiy/kulkov-sergiy.webp';
-import kulkovSergiy from '/pages/kulkov-sergiy/kulkov-sergiy.html';
+import kulkovSergiyImg from './pages/kulkov-sergiy/kulkov-sergiy.webp';
+import bakhtinovCaveliiImg from '/pages/bakhtinov-cavelii/IMG_2988.webp';
 
 const heroes = [
   {
     name: "АСТРАХАНЦЕВ ОЛЕКСАНДР",
     img: IMG_9682,
-    link: astrakhantsevOleksandr,
+    link: '/pages/astrakhantsev-oleksandr/astrakhantsev-oleksandr.html',
     desc: `
       МАЙСТЕР КІОКУШИНКАЙ КАРАТЕ <br>
       НАРОДИВСЯ 1969 РОКУ В М. БЕРДИЧІВ
     `,
   },
+     {
+    name: "Бахтiнoв Cавелій",
+    img: bakhtinovCaveliiImg,
+    link: '/pages/bakhtinov-cavelii/bakhtinov-cavelii.html',
+    desc: `
+      СПОРТСМЕН КІОКУШИНКАЙ КАРАТЕ ТА СПЕЦПРИЗНАЧИНЕЦЬ <br>
+      НАРОДИВСЯ 1979 РОКУ.`,
+  },
   {
     name: "КУЛЬКОВ СЕРГІЙ",
     img: kulkovSergiyImg,
-    link: kulkovSergiy,
+    link: '/pages/kulkov-sergiy/kulkov-sergiy.html',
     desc: `
       СПОРТСМЕН КІОКУШИНКАЙ КАРАТЕ ТА СПЕЦПРИЗНАЧИНЕЦЬ <br>
       НАРОДИВСЯ 1979 РОКУ.`,
   },
-   {
-    name: "КУЛЬКОВ СЕРГІЙ",
-    img: "/pages/kulkov-sergiy/kulkov-sergiy.webp",
-    link: "/pages/kulkov-sergiy/kulkov-sergiy.html",
-    desc: `
-      СПОРТСМЕН КІОКУШИНКАЙ КАРАТЕ ТА СПЕЦПРИЗНАЧИНЕЦЬ <br>
-      НАРОДИВСЯ 1979 РОКУ.`,
-  },
+ 
 ];
 
 
