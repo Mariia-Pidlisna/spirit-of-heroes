5cde0854f973f7f11d35ae0b069835c32cf2fcb2
Mariia-Pidlisna <mariia.pidlisna@gmail.com>
2025-10-16 06:19:22 +0000

    Deploying to gh-pages from @ Mariia-Pidlisna/spirit-of-heroes@8bebe433b6dcb684d81ccccdd0811767c6cc28e3 🚀

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function l(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function o(e){if(e.ep)return;e.ep=!0;const i=l(e);fetch(e.href,i)}})();const n="/spirit-of-heroes/astrakhantsev-oleksandr/IMG_9682.webp",a="/spirit-of-heroes/bakhtinov-cavelii/IMG_2988.webp",c="/spirit-of-heroes/kulkov-sergiy/kulkov-sergiy.webp",d="/spirit-of-heroes/bozhko-serhii/IMG_3011.webp",m="/spirit-of-heroes/vasin-valerii/IMG_9629.webp",u=[{name:"Астраханцев Олександр",img:n,link:"./astrakhantsev-oleksandr/astrakhantsev-oleksandr.html",desc:`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const o="/astrakhantsev-oleksandr/IMG_9682.webp",a="/bakhtinov-cavelii/IMG_2988.webp",c="/kulkov-sergiy/kulkov-sergiy.webp",m="/bozhko-serhii/IMG_3011.webp",d="/vasin-valerii/IMG_9629.webp",u="/assets/IMG_9701-Dum3jM24.webp",g=[{name:"Астраханцев Олександр",img:o,link:"./astrakhantsev-oleksandr/astrakhantsev-oleksandr.html",desc:`
       Майстер кіокушинкай карате<br>
       Народився 1969 року в м. Бердичів 
     `},{name:"Бахтiнoв Cавелій",img:a,link:"./bakhtinov-cavelii/bakhtinov-cavelii.html",desc:`
       Спортсмен кіокушинкай карате<br>
-      Народився 1999 року у м. Буськ`},{name:"Божко Сергій",img:d,link:"./bozhko-serhii/bozhko-serhii.html",desc:`
+      Народився 1999 року у м. Буськ`},{name:"Божко Сергій",img:m,link:"./bozhko-serhii/bozhko-serhii.html",desc:`
       Молодший сержант <br>
-      Народився 19 жовтня 1976 року`},{name:"Васін Валерій",img:m,link:"./vasin-valerii/vasin-valerii.html",desc:`
+      Народився 19 жовтня 1976 року`},{name:"Васін Валерій",img:d,link:"./vasin-valerii/vasin-valerii.html",desc:`
       Тренер з кіокушинкай карате<br>
-      Народився 1975 року, м. Херсон`},{name:"Кульков Cергій",img:c,link:"./kulkov-sergiy/kulkov-sergiy.html",desc:`
+      Народився 1975 року, м. Херсон`},{name:"Думанський Павло",img:u,link:"/dumanskyi-pavlo/dumanskyi-pavlo.html",desc:`
+      Спортсмен кіокушинкай карате<br>
+      Народився 2003 року у Львові`},{name:"Кульков Cергій",img:c,link:"./kulkov-sergiy/kulkov-sergiy.html",desc:`
       Спортсмен кіокушинкай карате та Спецпризначинець<br>
-      Народився 1979 року`}],f=document.querySelector(".gallery-ul");u.forEach(t=>{const r=document.createElement("li");r.classList.add("gallery-list"),r.innerHTML=`
-    <a class="gallery-list-url" href="${t.link}">
-      <img src="${t.img}" alt="${t.name}">
-      <h2 class="gallery-title-img">${t.name}</h2>
-      <p>${t.desc}</p>
+      Народився 1979 року`}],h=document.querySelector(".gallery-ul");g.forEach(r=>{const t=document.createElement("li");t.classList.add("gallery-list"),t.innerHTML=`
+    <a class="gallery-list-url" href="${r.link}">
+      <img src="${r.img}" alt="${r.name}">
+      <h2 class="gallery-title-img">${r.name}</h2>
+      <p>${r.desc}</p>
     </a>
-  `,f.appendChild(r)});
+  `,h.appendChild(t)});
 //# sourceMappingURL=index.js.map
