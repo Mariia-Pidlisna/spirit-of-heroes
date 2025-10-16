b8ae9f7d3050bc513833d3f67df9b1f737282e9b
Mariia Pidlisna <mariia.pidlisna@gmail.com>
2025-10-16 23:34:02 +0300

    var100500

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-      publicDir: 'src/public',
     build: {
       sourcemap: true,
       rollupOptions: {
