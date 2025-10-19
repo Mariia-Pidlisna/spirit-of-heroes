28319cd62c67273cc0ec812c65dcc9a1a3a6cfba
Mariia-Pidlisna <mariia.pidlisna@gmail.com>
2025-10-19 20:04:21 +0000

    Deploying to gh-pages from @ Mariia-Pidlisna/spirit-of-heroes@6968830233e65eb02a3371018d17b36db093e8ff 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const o="/astrakhantsev-oleksandr/IMG_9682.webp",a="/bakhtinov-cavelii/IMG_2988.webp",c="/kulkov-sergiy/kulkov-sergiy.webp",m="/bozhko-serhii/IMG_3011.webp",d="/vasin-valerii/IMG_9629.webp",u="/assets/IMG_9701-Dum3jM24.webp",g=[{name:"Астраханцев Олександр",img:o,link:"./partials/astrakhantsev-oleksandr.html",desc:`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=n(e);fetch(e.href,i)}})();const o="/astrakhantsev-oleksandr/IMG_9682.webp",a="/bakhtinov-cavelii/IMG_2988.webp",c="/kulkov-sergiy/kulkov-sergiy.webp",m="/bozhko-serhii/IMG_3011.webp",d="/vasin-valerii/IMG_9629.webp",u="/assets/IMG_9701-Dum3jM24.webp",g=[{name:"Астраханцев Олександр",img:o,link:"./astrakhantsev-oleksandr.html",desc:`
       Майстер кіокушинкай карате<br>
       Народився 1969 року в м. Бердичів 
     `},{name:"Бахтiнoв Cавелій",img:a,link:"./bakhtinov-cavelii/bakhtinov-cavelii.html",desc:`
@@ -11,11 +11,11 @@
       Спортсмен кіокушинкай карате<br>
       Народився 2003 року у Львові`},{name:"Кульков Cергій",img:c,link:"./kulkov-sergiy/kulkov-sergiy.html",desc:`
       Спортсмен кіокушинкай карате та Спецпризначинець<br>
-      Народився 1979 року`}],p=document.querySelector(".gallery-ul");g.forEach(r=>{const t=document.createElement("li");t.classList.add("gallery-list"),t.innerHTML=`
+      Народився 1979 року`}],f=document.querySelector(".gallery-ul");g.forEach(r=>{const t=document.createElement("li");t.classList.add("gallery-list"),t.innerHTML=`
     <a class="gallery-list-url" href="${r.link}">
       <img src="${r.img}" alt="${r.name}">
       <h2 class="gallery-title-img">${r.name}</h2>
       <p>${r.desc}</p>
     </a>
-  `,p.appendChild(t)});
+  `,f.appendChild(t)});
 //# sourceMappingURL=index.js.map
