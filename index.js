bbb3b4013a489d812f4c8e5c6dfc6d47d6553ff7
Mariia-Pidlisna <mariia.pidlisna@gmail.com>
2025-10-20 18:34:49 +0000

    Deploying to gh-pages from @ Mariia-Pidlisna/spirit-of-heroes@5c3127108449c73ee097bea81e5e8128df0e1531 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();const l="/astrakhantsev-oleksandr/IMG_9682.webp",a="/bakhtinov-cavelii/IMG_2988.webp",c="/kulkov-sergiy/kulkov-sergiy.webp",m="/bozhko-serhii/IMG_3011.webp",d="/vasin-valerii/IMG_9629.webp",k="/dumanskyi-pavlo/IMG_9701.webp",v="/zhydkov-mykola/IMG_9666.webp",u="/zvonyk-dan/IMG_9852.webp",h="/kostakov-serhii/IMG_2935.webp",y=[{name:"Астраханцев Олександр",img:l,link:"./astrakhantsev-oleksandr/astrakhantsev-oleksandr.html",desc:`
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function t(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=t(e);fetch(e.href,i)}})();const r="/astrakhantsev-oleksandr/IMG_9682.webp",a="/bakhtinov-cavelii/IMG_2988.webp",c="/kulkov-sergiy/IMG_9719.webp",m="/bozhko-serhii/IMG_3011.webp",d="/vasin-valerii/IMG_9629.webp",k="/dumanskyi-pavlo/IMG_9701.webp",y="/zhydkov-mykola/IMG_9666.webp",h="/zvonyk-dan/IMG_9852.webp",v="/kostakov-serhii/IMG_2935.webp",u="/malofieiev-oleksandr/IMG_9698.webp",g="/medynskyi-oleh/medynskyi-oleh-kostiantynovych.webp",p=[{name:"Астраханцев Олександр",img:r,link:"./astrakhantsev-oleksandr/astrakhantsev-oleksandr.html",desc:`
       Майстер кіокушинкай карате<br>
       Народився 1969 року в м. Бердичів 
     `},{name:"Бахтiнoв Cавелій",img:a,link:"./bakhtinov-cavelii/bakhtinov-cavelii.html",desc:`
@@ -9,19 +9,23 @@
       Тренер з кіокушинкай карате<br>
       Народився 1975 року, м. Херсон`},{name:"Думанський Павло",img:k,link:"./dumanskyi-pavlo/dumanskyi-pavlo.html",desc:`
       Спортсмен кіокушинкай карате<br>
-      Народився 2003 року у Львові`},{name:"Жидков Микола",img:v,link:"./zhydkov-mykola/zhydkov-mykola.html",desc:`
+      Народився 2003 року у Львові`},{name:"Жидков Микола",img:y,link:"./zhydkov-mykola/zhydkov-mykola.html",desc:`
       Людина, яка любила футбол та карате<br>
-      Народився 2001 року м. Кропивницький`},{name:"Звоник Дан",img:u,link:"./zvonyk-dan/zvonyk-dan.html",desc:`
+      Народився 2001 року м. Кропивницький`},{name:"Звоник Дан",img:h,link:"./zvonyk-dan/zvonyk-dan.html",desc:`
       Спортсмен кіокушинкай карате<br>
-      Народився 1996 року м. Донецьк`},{name:"Костаков Сергій",img:h,link:"./kostakov-serhii/kostakov-serhii.html",desc:`
+      Народився 1996 року м. Донецьк`},{name:"Костаков Сергій",img:v,link:"./kostakov-serhii/kostakov-serhii.html",desc:`
       Старший солдат, Снайпер<br>
       Народився 12 травня 1972 року в м. Київ`},{name:"Кульков Cергій",img:c,link:"./kulkov-sergiy/kulkov-sergiy.html",desc:`
       Спортсмен кіокушинкай карате та Спецпризначинець<br>
-      Народився 1979 року`}],g=document.querySelector(".gallery-ul");y.forEach(t=>{const o=document.createElement("li");o.classList.add("gallery-list"),o.innerHTML=`
-    <a class="gallery-list-url" href="${t.link}">
-      <img src="${t.img}" alt="${t.name}">
-      <h2 class="gallery-title-img">${t.name}</h2>
-      <p>${t.desc}</p>
+      Народився 1979 року`},{name:"Малофєєв Олександр",img:u,link:"./malofieiev-oleksandr/malofieiev-oleksandr.html",desc:`
+      Майстер кіокушинкай карате, 1 дан<br>
+      Народився 1975 року`},{name:"Мединський Олег",img:g,link:"./medynskyi-oleh/medynskyi-oleh.html",desc:`
+      Майстер кіокушинкай карате, 2 дан<br>
+      Народився 1968 року в с. Іллінка`}],b=document.querySelector(".gallery-ul");p.forEach(o=>{const n=document.createElement("li");n.classList.add("gallery-list"),n.innerHTML=`
+    <a class="gallery-list-url" href="${o.link}">
+      <img src="${o.img}" alt="${o.name}">
+      <h2 class="gallery-title-img">${o.name}</h2>
+      <p>${o.desc}</p>
     </a>
-  `,g.appendChild(o)});
+  `,b.appendChild(n)});
 //# sourceMappingURL=index.js.map
